b18092e6238b0191f56ebc41b0c25979c5885c6c
Elena-Weber <crochetmiracles@gmail.com>
2021-06-07 18:07:22 -0400

    commit tree 62dd1b6e588e79bcd8c93dc9f2dd964ac06c0cc5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(arr, func) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const each = arr[i];
+        newArr.push(func(each));
+    }
+    return newArr;
+}
+function reduce(arr, func, startingValue=0) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < arr.length; i++) {
+        total = func(arr[i], total);
+    }
+    return total;
+    } else {
+        total = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+        total = func(arr[i], total);
+    }
+    return total;
+    }
+}
